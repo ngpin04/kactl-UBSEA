fec843aa860ecf022db03173301cf959d6d6fb34
Binh Nguyen <bnguyen5@student.ubc.ca>
2024-03-31 16:09:20 -0700

    added AhoCorasick tempalte

--- content/strings/AhoCorasick.h
+++ content/strings/AhoCorasick.h
@@ -1,85 +1,85 @@
 /**
- * Author: Simon Lindholm
- * Date: 2015-02-18
- * License: CC0
- * Source: marian's (TC) code
- * Description: Aho-Corasick automaton, used for multiple pattern matching.
- * Initialize with AhoCorasick ac(patterns); the automaton start node will be at index 0.
- * find(word) returns for each position the index of the longest word that ends there, or -1 if none.
- * findAll($-$, word) finds all words (up to $N \sqrt N$ many if no duplicate patterns)
- * that start at each position (shortest first).
- * Duplicate patterns are allowed; empty patterns are not.
- * To find the longest words that start at each position, reverse all input.
- * For large alphabets, split each symbol into chunks, with sentinel bits for symbol boundaries.
- * Time: construction takes $O(26N)$, where $N =$ sum of length of patterns.
- * find(x) is $O(N)$, where N = length of x. findAll is $O(NM)$.
- * Status: stress-tested
+ * Tested with https://www.acmicpc.net/problem/9250
  */
-#pragma once
 
-struct AhoCorasick {
-	enum {alpha = 26, first = 'A'}; // change this!
-	struct Node {
-		// (nmatches is optional)
-		int back, next[alpha], start = -1, end = -1, nmatches = 0;
-		Node(int v) { memset(next, v, sizeof(next)); }
-	};
-	vector<Node> N;
-	vi backp;
-	void insert(string& s, int j) {
-		assert(!s.empty());
-		int n = 0;
-		for (char c : s) {
-			int& m = N[n].next[c - first];
-			if (m == -1) { n = m = sz(N); N.emplace_back(-1); }
-			else n = m;
-		}
-		if (N[n].end == -1) N[n].start = j;
-		backp.push_back(N[n].end);
-		N[n].end = j;
-		N[n].nmatches++;
-	}
-	AhoCorasick(vector<string>& pat) : N(1, -1) {
-		rep(i,0,sz(pat)) insert(pat[i], i);
-		N[0].back = sz(N);
-		N.emplace_back(0);
+struct AHC {
+    struct node {
+        bool leaf;
+        array <int, 26> ptr;  // modifies this if binary
+        int link, par, pch;
 
-		queue<int> q;
-		for (q.push(0); !q.empty(); q.pop()) {
-			int n = q.front(), prev = N[n].back;
-			rep(i,0,alpha) {
-				int &ed = N[n].next[i], y = N[prev].next[i];
-				if (ed == -1) ed = y;
-				else {
-					N[ed].back = y;
-					(N[ed].end == -1 ? N[ed].end : backp[N[ed].start])
-						= N[y].end;
-					N[ed].nmatches += N[y].nmatches;
-					q.push(ed);
-				}
-			}
-		}
-	}
-	vi find(string word) {
-		int n = 0;
-		vi res; // ll count = 0;
-		for (char c : word) {
-			n = N[n].next[c - first];
-			res.push_back(N[n].end);
-			// count += N[n].nmatches;
-		}
-		return res;
-	}
-	vector<vi> findAll(vector<string>& pat, string word) {
-		vi r = find(word);
-		vector<vi> res(sz(word));
-		rep(i,0,sz(word)) {
-			int ind = r[i];
-			while (ind != -1) {
-				res[i - sz(pat[ind]) + 1].push_back(ind);
-				ind = backp[ind];
-			}
-		}
-		return res;
-	}
+        node (int _pch = 0, int _par = 0){
+            pch = _pch;
+            par = _par;
+            leaf = false;
+            for(int i = 0; i < 26; i++)  // modifies this if binary
+                ptr[i] = -1;
+            link = -1;
+        }
+    };
+    vector <node> tree;
+    int cnt;
+
+    AHC() {
+        cnt = 0;
+        tree.assign(1, node());
+    }
+
+    #define tr tree[u]
+
+    bool &leaf(int u) {
+        return tr.leaf;
+    }
+
+    int &ptr(int u, int c) {
+        return tr.ptr[c];
+    }
+
+    int &pch(int u) {
+        return tr.pch;
+    }
+
+    int &link(int u) {
+        return tr.link;
+    }
+
+    int &par(int u) {
+        return tr.par;
+    }
+    
+    #undef tr
+
+    void add(string s) {
+        int cur = 0;
+        for(int i = 0; i < (int) s.size(); i++){
+            int val = s[i] - 'a';  // modifies this 
+            if (ptr(cur, val) < 0) {
+                ptr(cur, val) = ++cnt;
+                tree.push_back(node(val, cur));
+            }
+            cur = ptr(cur, val);
+        }
+        leaf(cur) = true;
+    }
+
+    int getptr(int u, int c) { 
+        int &res = ptr(u, c);
+        if(res < 0) {
+            if(u == 0)
+                return res = 0;
+            return res = getptr(getlink(u), c);
+        }
+        return res;
+    }
+
+    int getlink(int u) {
+        int &res = link(u);
+        if(res < 0) {
+            if(u == 0 || par(u) == 0)
+                res = 0;
+            else
+                res = getptr(getlink(par(u)), pch(u));
+        }
+        return res;
+    }
 };
