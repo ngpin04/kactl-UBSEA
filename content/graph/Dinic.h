42fa00529252cb90d02f710395972bddf51f3fc7
Binh Nguyen <bnguyen5@student.ubc.ca>
2024-04-04 18:34:01 -0700

    Added Dinic.h

--- content/graph/Dinic.h
+++ content/graph/Dinic.h
@@ -1,53 +1,104 @@
 /**
- * Author: chilli
- * Date: 2019-04-26
- * License: CC0
- * Source: https://cp-algorithms.com/graph/dinic.html
- * Description: Flow algorithm with complexity $O(VE\log U)$ where $U = \max |\text{cap}|$.
- * $O(\min(E^{1/2}, V^{2/3})E)$ if $U = 1$; $O(\sqrt{V}E)$ for bipartite matching.
- * Status: Tested on SPOJ FASTFLOW and SPOJ MATCHING, stress-tested
+ * Tested with https://cses.fi/problemset/task/1694/
+ * 
  */
 #pragma once
 
 struct Dinic {
 	struct Edge {
-		int to, rev;
-		ll c, oc;
-		ll flow() { return max(oc - c, 0LL); } // if you need flows
+		int v, pos; long long flow, cap;
+ 
+		Edge(){}
+		Edge(int _v, int _pos, long long _flow, long long _cap) {
+			v = _v;
+			pos = _pos;
+			flow = _flow;
+			cap = _cap;
+		}
 	};
-	vi lvl, ptr, q;
-	vector<vector<Edge>> adj;
-	Dinic(int n) : lvl(n), ptr(n), q(n), adj(n) {}
-	void addEdge(int a, int b, ll c, ll rcap = 0) {
-		adj[a].push_back({b, sz(adj[b]), c, c});
-		adj[b].push_back({a, sz(adj[a]) - 1, rcap, rcap});
+ 
+	int n;
+	vector <vector <Edge>> adj;
+	vector <int> ptr;
+	vector <int> lev;
+ 
+	Dinic (int _n) {
+		n = _n;
+		adj.assign(n + 5, vector <Edge> (0));
+		ptr.assign(n + 5, 0);
+		lev.assign(n + 5, 0);
 	}
-	ll dfs(int v, int t, ll f) {
-		if (v == t || !f) return f;
-		for (int& i = ptr[v]; i < sz(adj[v]); i++) {
-			Edge& e = adj[v][i];
-			if (lvl[e.to] == lvl[v] + 1)
-				if (ll p = dfs(e.to, t, min(f, e.c))) {
-					e.c -= p, adj[e.to][e.rev].c += p;
-					return p;
-				}
+ 
+	void addEgde(int u, int v, long long cap, long long rcap = 0) {
+		Edge a = Edge(v, adj[v].size(), 0, cap);
+		Edge b = Edge(u, adj[u].size(), 0, rcap);
+		adj[u].push_back(a);
+		adj[v].push_back(b);
+	}
+ 
+	bool bfs(int s, int t) {
+		ptr = lev = vector <int> (n + 5, 0);
+ 
+		lev[s] = 1;
+ 
+		queue <int> q;
+		q.push(s);
+ 
+		while (q.size()) {
+			int u = q.front();
+			q.pop();
+ 
+			for (Edge e : adj[u]) if (e.flow < e.cap && lev[e.v] == 0) {
+				lev[e.v] = lev[u] + 1;
+				q.push(e.v);
+			}
+		}
+		//cerr << lev[t] << endl;
+		return lev[t] > 0;
+	}
+ 
+	int dfs(int u, int flow, int t) {
+		if (u == t)
+			return flow;
+		for (int &i = ptr[u]; i < (int) adj[u].size(); i++) {
+			Edge &e = adj[u][i];
+ 
+			if (lev[u] + 1 != lev[e.v] || e.flow == e.cap) 
+				continue;
+ 
+			int new_flow = min(0LL + flow, e.cap - e.flow);
+			if (int val = dfs(e.v, new_flow, t)) {
+				e.flow += val;
+				adj[e.v][e.pos].flow -= val;
+				return val;
+			}
 		}
 		return 0;
 	}
-	ll calc(int s, int t) {
-		ll flow = 0; q[0] = s;
-		rep(L,0,31) do { // 'int L=30' maybe faster for random data
-			lvl = ptr = vi(sz(q));
-			int qi = 0, qe = lvl[s] = 1;
-			while (qi < qe && !lvl[t]) {
-				int v = q[qi++];
-				for (Edge e : adj[v])
-					if (!lvl[e.to] && e.c >> (30 - L))
-						q[qe++] = e.to, lvl[e.to] = lvl[v] + 1;
-			}
-			while (ll p = dfs(s, t, LLONG_MAX)) flow += p;
-		} while (lvl[t]);
-		return flow;
+ 
+	long long maxFlow(int s, int t) {
+		long long res = 0;
+ 
+		while (bfs(s, t)) {
+ 
+			while (int val = dfs(s, (int) 1e9, t)) 
+				res += val;
+		}
+		return res;
 	}
-	bool leftOfMinCut(int a) { return lvl[a] != 0; }
+
+	// If mincut is needed
+	void mincut(int s, int t) {
+		int res = 0;
+		while (bfs(s, t)) 
+			while (int val = dfs(s, t, (int) 1e9))
+				res += val;
+		cout << res << "\n";
+		bfs(s, t);
+		for (int i = 1; i <= n; i++) 
+		for (Edge e : adj[i])
+			if (dis[i] > 0 && dis[e.v] == 0)
+				cout << i << " " << e.v << "\n";
+	}
 };
+
