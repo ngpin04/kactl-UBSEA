44859c0440bb37dd44be9fbc6df78fb80beebb2e
Binh Nguyen <bnguyen5@student.ubc.ca>
2024-09-04 19:20:09 -0700

    update FFTmod

--- content/numerical/FastFourierTransformMod.h
+++ content/numerical/FastFourierTransformMod.h
@@ -13,6 +13,7 @@
  */
 #pragma once
 
+typedef long long ll;
 typedef complex<double> C;
 
 void fft(vector<C>& a) {
@@ -35,8 +36,9 @@ void fft(vector<C>& a) {
 	for (int k = 1; k < n; k *= 2)
 	for (int i = 0; i < n; i += 2 * k)  
 	for (int j = 0; j < k; j++) {
-		auto x = (double *)&rt[j+k], y = (double *)&a[i+j+k];	
-		C z(x[0]*y[0] - x[1]*y[1], x[0]*y[1] + x[1]*y[0]);		
+		// C z = rt[j+k] * a[i+j+k]; // (25% faster if hand-rolled)  /// include-line
+		auto x = (double *)&rt[j+k], y = (double *)&a[i+j+k];		/// exclude-line
+		C z(x[0]*y[0] - x[1]*y[1], x[0]*y[1] + x[1]*y[0]);		   /// exclude-line
 		a[i + j + k] = a[i + j] - z;
 		a[i + j] += z;
 	}
@@ -44,20 +46,29 @@ void fft(vector<C>& a) {
 
 typedef vector<ll> vl;
 template<int M> vl convMod(const vl &a, const vl &b) {
-	if (a.empty() || b.empty()) return {};
-	vl res(sz(a) + sz(b) - 1);
-	int B=32-__builtin_clz(sz(res)), n=1<<B, cut=int(sqrt(M));
+
+	vl res((int) a.size() + b.size() - 1);
+	int B=32-__builtin_clz(res.size()), n=1<<B, cut=int(sqrt(M));
+	
 	vector<C> L(n), R(n), outs(n), outl(n);
-	rep(i,0,sz(a)) L[i] = C((int)a[i] / cut, (int)a[i] % cut);
-	rep(i,0,sz(b)) R[i] = C((int)b[i] / cut, (int)b[i] % cut);
+
+	for (int i = 0; i < (int) a.size(); i++) 
+		L[i] = C((int)a[i] / cut, (int)a[i] % cut);
+
+	for (int i = 0; i < (int) b.size(); i++)  
+		R[i] = C((int)b[i] / cut, (int)b[i] % cut);
+	
 	fft(L), fft(R);
-	rep(i,0,n) {
+
+	for (int i = 0; i < n; i++) {
 		int j = -i & (n - 1);
 		outl[j] = (L[i] + conj(L[j])) * R[i] / (2.0 * n);
 		outs[j] = (L[i] - conj(L[j])) * R[i] / (2.0 * n) / 1i;
 	}
+
 	fft(outl), fft(outs);
-	rep(i,0,sz(res)) {
+	
+	for (int i = 0; i < (int) res.size(); i++) {
 		ll av = ll(real(outl[i])+.5), cv = ll(imag(outs[i])+.5);
 		ll bv = ll(imag(outl[i])+.5) + ll(real(outs[i])+.5);
 		res[i] = ((av % M * cut + bv) % M * cut + cv) % M;
